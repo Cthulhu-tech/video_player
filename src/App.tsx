60e748b35261f9372408728ccfec49b876323931
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2022-08-05 12:55:26 +0300

    app add

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,10 @@ export const App = () => {
   return <HashRouter>
     <Routes>
       <Route element={<Layout/>}>
-        <Route path="/" element={<div>ddsadsds</div>}/>
+        <Route path="/" element={<div>home</div>}/>
+        <Route path="/last" element={<div>last</div>}/>
+        <Route path="/noted" element={<div>noted</div>}/>
+        <Route path="/stats" element={<div>stats</div>}/>
       </Route>
     </Routes>
   </HashRouter>
--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,8 @@ import reportWebVitals from './reportWebVitals';
 import ReactDOM from 'react-dom/client';
 import './style/style.scss';
 import { App } from './App';
-import React from 'react';
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<App />);
 
 reportWebVitals();
