acbb26df1809675b788a58cabf093940a6efcff4
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2022-08-05 12:54:07 +0300

    layout add

--- src/compopnents/layouts/layout.tsx
+++ src/compopnents/layouts/layout.tsx
@@ -1,12 +1,17 @@
 import { Outlet } from 'react-router-dom';
-import { Header } from './header';
+import { Footer } from './footer/footer';
+import { Header } from './header/header';
+
 import './layout.scss';
 
 export const Layout = () => {
 
     return <>
         <Header/>
-        <Outlet/>
+        <main>
+            <Outlet/>
+        </main>
+        <Footer/>
     </>
 
 }
