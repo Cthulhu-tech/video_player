d5669681cfd25a673c406eb0b6cbd00edb808604
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2022-08-06 22:04:31 +0300

    update video logic

--- src/compopnents/layouts/video/video.tsx
+++ src/compopnents/layouts/video/video.tsx
@@ -1,10 +1,14 @@
 import { ReduxStore } from '../../../interface/reduxInterface';
-import { useRef, useEffect } from 'react';
-import { useSelector } from 'react-redux';
+import { videoLength, videoPosition } from '../../../redux/store/videoState';
+import { useSelector, useDispatch } from 'react-redux';
+import { useRef, useEffect, useState } from 'react';
 
 export const Video = (video: {src: string}) => {
 
+    const dispatch = useDispatch();
     const videoRef = useRef<HTMLVideoElement>(null);
+    const [_canPlay, setPlay] = useState<CanPlayTypeResult>();
+    const _video = useSelector((store:ReduxStore) => store.File);
     const videoState = useSelector((store:ReduxStore) => store.VideoStore);
 
     useEffect(() => {
@@ -21,13 +25,31 @@ export const Video = (video: {src: string}) => {
 
             }
 
+            videoRef.current.volume = videoState.audio;
             videoRef.current.playbackRate = videoState.speed;
             videoRef.current.playbackRate = videoState.speed;
 
         }
 
-    },[videoState]);
+    },[videoRef, videoState.audio, videoState.speed, videoState.videoLength, videoState.play, _video]);
 
-    return <video className="video-player__video" ref={videoRef} src={video.src} width="100%" height="100%" />
+    useEffect(() => {
+        
+        if(videoRef.current && videoState.videoPosition.change)
+            videoRef.current.currentTime = videoState.videoPosition.position;
+
+    }, [videoState.videoPosition])
+
+    const timeHandler = () => videoRef.current && dispatch(videoPosition({change: false, position: videoRef.current.currentTime}));
+    const handleLoadedMetadata = () => videoRef.current && dispatch(videoLength(videoRef.current.duration));
+
+    return  <video 
+                onTimeUpdate={timeHandler}
+                onLoadedMetadata={handleLoadedMetadata}
+                className="video-player__video" 
+                ref={videoRef} 
+                src={video.src} 
+                width="100%" height="100%"
+            />
 
 }
