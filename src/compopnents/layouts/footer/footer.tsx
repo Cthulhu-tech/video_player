ecdaf58ac492801293108a6bceb8652c21bf2920
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2022-08-06 22:05:18 +0300

    update logic hover and logic active

--- src/compopnents/layouts/footer/footer.tsx
+++ src/compopnents/layouts/footer/footer.tsx
@@ -1,43 +1,54 @@
 import { ReduxStore } from '../../../interface/reduxInterface';
 import { useVideoTrack } from '../../hook/useVideoTrack';
 import { useSelector } from 'react-redux';
-import './footer.scss';
 import { useEffect } from 'react';
+import './footer.scss';
 
 export const Footer = () => {
 
+    const _video = useSelector((store:ReduxStore) => store.File);
     const videoState = useSelector((store:ReduxStore) => store.VideoStore);
 
-    const {prev, next, first, last, play, pause} = useVideoTrack();
+    const {prev, next, first, last, play, pause, changeAudio, cahngeVideo, down, up} = useVideoTrack();
 
-    useEffect(() => {console.log(videoState)},[videoState]);
+    useEffect(() => {},[videoState]);
 
     return <footer className="footer">
+            <input className="footer__control" type="range" max={videoState.videoLength} value={videoState.videoPosition.position} onMouseUp={up} onMouseDown={down} onChange={cahngeVideo}/>
         <div className="footer__button-container">
-            <div className="button-player" onClick={play}>
-                <div className="footer__button play"></div>
+            <div className="sub-footer__button-container">
+                <div className={videoState.play && _video.file.length > 0 ? "button-player active-button" : _video.file.length > 0 ? "button-player active-hover" : "button-player"} onClick={play}>
+                    <div className={_video.file.length > 0 ? "footer__button play active " : "footer__button play"}></div>
+                </div>
+                <div className={!videoState.play && _video.file.length > 0 ? "button-player active-button" : _video.file.length > 0 ? "button-player active-hover" : "button-player"} onClick={pause}>
+                    <div className={_video.file.length > 0 ? "footer__button active stop" : "footer__button stop"}></div>
+                </div>            
+                <div className="footer__button"></div>
             </div>
-            <div className="button-player" onClick={pause}>
-                <div className="footer__button stop"></div>
-            </div>            
-            <div className="footer__button"></div>
-        </div>
-        <div className="footer__button-container">
-            <div className="button-player" onClick={first}>
-                <div className="footer__button first"></div>
+            <div className="sub-footer__button-container">
+                <div className={_video.file.length > 1 ? "button-player active-hover" : "button-player"} onClick={first}>
+                    <div className={_video.file.length > 1 ? "footer__button first" : "footer__button first diactive"}></div>
+                </div>
+                <div className={_video.file.length > 1 ? "button-player active-hover" : "button-player"} onClick={prev}>
+                    <div className={_video.file.length > 1 ? "footer__button prev active" : "footer__button prev"}></div>
+                </div>
+                <div className={_video.file.length > 1 ? "button-player active-hover" : "button-player"} onClick={next}>
+                    <div className={_video.file.length > 1 ? "footer__button next active" : "footer__button next"}></div>
+                </div>
+                <div className={_video.file.length > 1 ? "button-player active-hover" : "button-player"} onClick={last}>
+                    <div className={_video.file.length > 1 ? "footer__button first right" : "footer__button first diactive right"}></div>
+                </div>
             </div>
-            <div onClick={prev} className="button-player">
-                <div className="footer__button prev"></div>
+            <div className="sub-footer__button-container">
+                <div className={_video.file.length > 1 ? "button-player active-hover" : "button-player"}>
+
+                </div>
             </div>
-            <div onClick={next} className="button-player">
-                <div className="footer__button next"></div>
+            <div className="audio-slider-container">
+                <div className="audio-range-shadow">
+                    <input type="range" className="audio-range" step="0.01" min="0" max="1" value={videoState.audio} onChange={changeAudio}/>
+                </div>
             </div>
-            <div className="button-player" onClick={last}>
-                <div className="footer__button first right"></div>
-            </div>
-        </div>
-        <div className="footer__button-container">
-            <div className="footer__button"></div>
         </div>
     </footer>
 
