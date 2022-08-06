c203e19da760c380357b3b796595a2158241acc2
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2022-08-06 22:03:50 +0300

    videostate update

--- src/redux/store/videoState.tsx
+++ src/redux/store/videoState.tsx
@@ -1,16 +1,24 @@
-import { IAction, VideoState } from "../../interface/reduxInterface";
+import { IAction, videoPositionType, VideoState } from "../../interface/reduxInterface";
 
 const defaultState:VideoState = {
 
     play: false,
     speed: 1,
-    audio: 100
+    videoPosition: {
+        change: false,
+        position: 0,
+    },
+    audio: 1,
+    videoLength: 0,
 
 }
 
 const playVideo = "play";
 const speedVideo = "speed";
 const audioVideo = "audio";
+
+const _videoLength = "videoLength";
+const _videoPosition = "positionVideo";
 
 export const VideoStateStore = (state = defaultState, action:IAction<string, boolean | number>) => {
     switch (action.type){
@@ -20,6 +28,10 @@ export const VideoStateStore = (state = defaultState, action:IAction<string, boo
             return {...state, speed: action.payload}
         case audioVideo:
             return {...state, audio: action.payload}
+        case _videoLength:
+            return {...state, videoLength: action.payload}
+        case _videoPosition:
+            return {...state, videoPosition: action.payload}
         default:
             return state;
     }
@@ -28,3 +40,6 @@ export const VideoStateStore = (state = defaultState, action:IAction<string, boo
 export const playVideoState = (payload: boolean) => ({ type: playVideo, payload });
 export const speedVideoState = (payload: number) => ({ type: speedVideo, payload });
 export const audioVideoState = (payload: number) => ({ type: audioVideo, payload });
+
+export const videoLength = (payload: number) => ({ type: _videoLength, payload });
+export const videoPosition = (payload: videoPositionType) => ({ type: _videoPosition, payload });
